25c7c14a3548a38c7c7e0f65c631e65452e989fd
Роман Дикарев <super.romanchik228@yandex.ru>
2025-08-18 21:35:10 +0300

    добавил новую таблицу WbLk

--- backend/wb/admin.py
+++ backend/wb/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Groups
+from .models import Groups, WbLk
 
 
 @admin.register(Groups)
 class GroupsAdmin(admin.ModelAdmin):
     list_display = ('name', 'permissions')
+
+
+@admin.register(WbLk)
+class WbLkAdmin(admin.ModelAdmin):
+    list_display = ('name', 'groups', 'inn')
--- backend/wb/models.py
+++ backend/wb/models.py
@@ -8,3 +8,19 @@ class Groups(models.Model):
 
     def __str__(self):
         return self.name
+
+
+# Модель для таблицы wb_lk
+class WbLk(models.Model):
+    # myapp_wblk
+    groups = models.ForeignKey(Groups, on_delete=models.CASCADE, null=True) #groups_id в бд
+    name = models.CharField(max_length=255)
+    token = models.CharField(max_length=400)
+    number = models.BigIntegerField(default=0, null=True)
+    cookie = models.TextField(default='', null=True)
+    authorizev3 = models.TextField(default='', null=True)
+    inn = models.BigIntegerField(default=0, null=True)
+    tg_id = models.BigIntegerField(default=0, null=True)
+
+    def __str__(self):
+        return self.name
