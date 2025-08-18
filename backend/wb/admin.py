2d8cac2ddc84ff6107d531cddfd56cc13a635528
Роман Дикарев <super.romanchik228@yandex.ru>
2025-08-18 21:40:36 +0300

    добавил новую таблицу nmids

--- backend/wb/admin.py
+++ backend/wb/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Groups, WbLk
+from .models import Groups, WbLk, nmids
 
 
 @admin.register(Groups)
@@ -10,3 +10,15 @@ class GroupsAdmin(admin.ModelAdmin):
 @admin.register(WbLk)
 class WbLkAdmin(admin.ModelAdmin):
     list_display = ('name', 'groups', 'inn')
+
+
+@admin.register(nmids)
+class NmidsAdmin(admin.ModelAdmin):
+    list_display = (
+        'nmid', 'title', 'brand', 'vendorcode', 'subjectname', 'needkiz',
+        'lk', 'created_at', 'updated_at', 'added_db'
+    )
+    list_filter = ('brand', 'subjectname', 'needkiz', 'lk')
+    search_fields = ('nmid', 'vendorcode', 'title', 'brand', 'nmuuid')
+    ordering = ('-added_db',)
+    date_hierarchy = 'added_db'
--- backend/wb/models.py
+++ backend/wb/models.py
@@ -24,3 +24,33 @@ class WbLk(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class nmids(models.Model):
+    lk = models.ForeignKey(WbLk, on_delete=models.CASCADE, default=1) #lk_id в бд
+    nmid = models.IntegerField() # Артикул WB
+    imtid = models.IntegerField() # ID карточки товара. Артикулы WB из одной карточки товара будут иметь одинаковый imtID
+    nmuuid = models.CharField(max_length=255) # Внутренний технический ID товара
+    subjectid = models.IntegerField() # ID предмета
+    subjectname = models.CharField(max_length=255) # Название предмета
+    vendorcode = models.CharField(max_length=255) # Артикул продавца
+    brand = models.CharField(max_length=255) # Бренд
+    title = models.CharField(max_length=500) # Наименование товара
+    description = models.TextField() # Описание товара
+    needkiz = models.BooleanField() # Требуется ли код маркировки для этого товара
+    dimensions = models.JSONField() # Габариты и вес товара c упаковкой, см и кг
+    characteristics = models.JSONField() # Характеристики
+    sizes = models.JSONField() # Размеры товара
+    tag_ids = models.JSONField(default=list)
+    created_at = models.DateTimeField() # Дата создания карточки товара (по данным WB)
+    updated_at = models.DateTimeField() # Дата изменения карточки товара (по данным WB)
+    added_db = models.DateTimeField(auto_now_add=True) # по МСК
+    is_active = models.BooleanField(default=True) # поле для понимания нужен товар или нет
+
+    class Meta:
+        unique_together = ['nmid', 'lk']
+        verbose_name = "Товар WB"
+        verbose_name_plural = "Товары WB"
+
+    def __str__(self):
+        return f"{self.nmid} – {self.title} ({self.brand})"
