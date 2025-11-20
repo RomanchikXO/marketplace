35be8e25246361fe93a432804288424e4388ab56
Роман Дикарев <super.romanchik228@yandex.ru>
2025-11-20 16:00:19 +0300

    добавлен таск сбора

--- backend/logger/tasks.py
+++ backend/logger/tasks.py
@@ -4,7 +4,7 @@ from context_logger import ContextLogger
 import logging
 from celery import shared_task
 from decorators import with_task_context
-from parsers.wildberies import get_orders, get_nmids, get_stocks_data_2_weeks
+from parsers.wildberies import get_orders, get_nmids, get_stocks_data_2_weeks, get_stat_products
 import asyncio
 
 logger = ContextLogger(logging.getLogger("logger"))
@@ -38,3 +38,11 @@ def get_stocks_to_db():
     logger.info("🟢 Обновляем остатки в DB")
     asyncio.run(get_stocks_data_2_weeks())
     logger.info("Остатки в DB обновлены")
+
+
+@shared_task
+@with_task_context("get_stat_products_task")
+def get_stat_products_task():
+    logger.info("🟢 Обновляем стату по товарам в БД")
+    asyncio.run(get_stat_products())
+    logger.info("Стата по товарам в БД обновлены")
